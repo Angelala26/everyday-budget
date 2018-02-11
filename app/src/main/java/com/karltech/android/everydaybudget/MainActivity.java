2f1e7091380cd86af5764021192ab9fa99e9f50e
Angelala26 <timphGH!ih1c>
2018-02-11 16:54:03 -0500

    Initial commit

---
+++ app/src/main/java/com/karltech/android/everydaybudget/MainActivity.java
@@ -0,0 +1,13 @@
+package com.karltech.android.everydaybudget;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
