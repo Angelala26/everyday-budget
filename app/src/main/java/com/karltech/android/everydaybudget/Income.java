c2c9da45cc8141a008f9af66a05816257478a1cd
Angelala26 <timphGH!ih1c>
2019-01-16 17:44:16 +0100

    update income.java with a textview that is created when someone adds income with the button

--- app/src/main/java/com/karltech/android/everydaybudget/Income.java
+++ app/src/main/java/com/karltech/android/everydaybudget/Income.java
@@ -3,14 +3,19 @@ package com.karltech.android.everydaybudget;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class Income extends AppCompatActivity {
 
     EditText enterIncomeEditText;
     EditText enterIncomeNamesEditText;
     Button addIncomeButton;
+
+    //TODO: show income on line after entered and leave space to enter more incomes
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +27,23 @@ public class Income extends AppCompatActivity {
         enterIncomeNamesEditText = findViewById(R.id.enter_expense_names_edit_text);
         addIncomeButton = findViewById(R.id.add_income_button);
 
+        //create new TextView for later
+        final TextView incomeDisplay = new TextView(this);
+
+
         addIncomeButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //convert to string or double
                 String income = enterIncomeEditText.getText().toString();
-                String incomeName = enterIncomeNamesEditText.getText().toString();            }
+                String incomeName = enterIncomeNamesEditText.getText().toString();
+                Double incomeDouble = Double.valueOf(income);
+
+                //TODO: Transfer this info to line in scroll view showing incomes
+                //create TextView after clicking
+                incomeDisplay.setLayoutParams(new RelativeLayout.LayoutParams
+                        (ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+                incomeDisplay.setText(income + "   " + incomeName);
+            }
         });
 
     }
