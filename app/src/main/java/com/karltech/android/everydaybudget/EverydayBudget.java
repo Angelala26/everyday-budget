590202ffc58e2bc9551bbc7c65bb7c6d93c9557f
Angelala26 <timphGH!ih1c>
2019-03-02 18:12:13 +0100

    Fix EverydayBudget to have navigation drawer

--- app/src/main/java/com/karltech/android/everydaybudget/EverydayBudget.java
+++ app/src/main/java/com/karltech/android/everydaybudget/EverydayBudget.java
@@ -1,12 +1,14 @@
 package com.karltech.android.everydaybudget;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.NavigationView;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import java.text.DecimalFormat;
 
-public class EverydayBudget extends AppCompatActivity {
+public class EverydayBudget extends MainActivity {
 
     TextView amountPerDayTextView;
     TextView regularIncomeTextView;
@@ -23,6 +25,52 @@ public class EverydayBudget extends AppCompatActivity {
         regularIncomeTextView = findViewById(R.id.regular_income_text_view);
         recurringExpensesTextView = findViewById(R.id.recurring_expenses_text_view);
         bigExpensesTextView = findViewById(R.id.big_expenses_text_view);
+
+        //add everything for navigation drawer
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        int id = menuItem.getItemId();
+                        switch (id) {
+                            case R.id.nav_expenses:
+                                Intent expenseIntent = new Intent
+                                        (EverydayBudget.this, Expenses.class);
+                                startActivity(expenseIntent);
+                                break;
+                            case R.id.nav_income:
+                                Intent incomeIntent = new Intent
+                                        (EverydayBudget.this, Income.class);
+                                startActivity(incomeIntent);
+                                break;
+                            case R.id.nav_home:
+                                Intent homeIntent = new Intent
+                                        (EverydayBudget.this, MainActivity.class);
+                                startActivity(homeIntent);
+                                break;
+                            case R.id.nav_large_purchases:
+                                Intent budgetIntent = new Intent
+                                        (EverydayBudget.this, EverydayBudget.class);
+                                startActivity(budgetIntent);
+                                break;
+                            case R.id.nav_transactions:
+                                Intent transactionsIntent = new Intent
+                                        (EverydayBudget.this, Transactions.class);
+                                startActivity(transactionsIntent);
+                                break;
+                        }
+                        //not sure if i should return false or true here
+                        return true;
+                    }
+                });
 
         }
 
