95759b6e4fdbfd62008e559fbbc937582e466e2a
Angelala26 <timphGH!ih1c>
2019-03-02 18:25:06 +0100

    Add navigation drawer in Transactions

--- app/src/main/java/com/karltech/android/everydaybudget/Transactions.java
+++ app/src/main/java/com/karltech/android/everydaybudget/Transactions.java
@@ -1,28 +1,62 @@
 package com.karltech.android.everydaybudget;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.view.MenuItem;
 
-public class Transactions extends AppCompatActivity {
+public class Transactions extends MainActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_transactions);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+        //add everything for navigation drawer
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        int id = menuItem.getItemId();
+                        switch (id) {
+                            case R.id.nav_expenses:
+                                Intent expenseIntent = new Intent
+                                        (Transactions.this, Expenses.class);
+                                startActivity(expenseIntent);
+                                break;
+                            case R.id.nav_income:
+                                Intent incomeIntent = new Intent
+                                        (Transactions.this, Income.class);
+                                startActivity(incomeIntent);
+                                break;
+                            case R.id.nav_home:
+                                Intent homeIntent = new Intent
+                                        (Transactions.this, MainActivity.class);
+                                startActivity(homeIntent);
+                                break;
+                            case R.id.nav_large_purchases:
+                                Intent budgetIntent = new Intent
+                                        (Transactions.this, EverydayBudget.class);
+                                startActivity(budgetIntent);
+                                break;
+                            case R.id.nav_transactions:
+                                Intent transactionsIntent = new Intent
+                                        (Transactions.this, Transactions.class);
+                                startActivity(transactionsIntent);
+                                break;
+                        }
+                        //not sure if i should return false or true here
+                        return true;
+                    }
+                });
     }
 }
